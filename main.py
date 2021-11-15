3a70fae6a47732917f1a67b0b05be7e280c48f32
krishivpiduri2010 <59217701+krishivpiduri2010@users.noreply.github.com>
2021-11-15 15:40:13 -0800

    Initial commit

---
+++ main.py
@@ -0,0 +1,82 @@
+import pygame
+
+WIDTH, HEIGHT = 1500, 800
+WIN = pygame.display.set_mode((WIDTH, HEIGHT))
+square_size = 50
+grid = [[0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+        [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+        [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+        [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+        [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+        [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+        [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+        [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+        [1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1],
+        [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+        [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+        [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+        [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+        [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+        [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+        [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+        [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+        [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+        [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+        [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+        [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+        [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+        [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+        [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+        [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+        [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+        [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+        [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+        [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+        [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]]
+
+
+def draw_grid(win: pygame.Surface):
+    for x in range(WIDTH, square_size):
+        pygame.draw.line(win, (0, 0, 0), (x, 0), (x, HEIGHT))
+    for y in range(HEIGHT, square_size):
+        pygame.draw.line(win, (0, 0, 0), (0, y), (WIDTH, y))
+
+
+def draw(win: pygame.Surface, grid_x, grid_y):
+    for row_pos, row in enumerate(range(grid_x, len(grid))):
+        for col_pos, cell in enumerate(range(grid_y, len(grid[row]))):
+            if grid[row][cell] == 1:
+                pygame.draw.rect(win, (255, 255, 255,),
+                                 (row_pos * square_size, col_pos * square_size, square_size, square_size))
+    draw_grid(win)
+    pygame.display.update()
+
+
+def main():
+    grid_x = 0
+    grid_y = 0
+    run = True
+    clock = pygame.time.Clock()
+    while run:
+        clock.tick(10)
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                run = False
+                break
+        keys = pygame.key.get_pressed()
+        if keys[pygame.K_LEFT] and grid_x > 0:
+            grid_x -= 1
+        if keys[pygame.K_RIGHT]:
+            grid_x += 1
+        if keys[pygame.K_UP] and grid_y > 0:
+            grid_y -= 1
+        if keys[pygame.K_DOWN]:
+            grid_y += 1
+        print(grid_x, grid_y)
+        draw(WIN, grid_x, grid_y)
+    pygame.quit()
+
+
+if __name__ == '__main__':
+    print(grid)
+    main()
