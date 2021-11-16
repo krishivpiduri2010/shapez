bedaf175e1497edaf6827292a651a606a9e2d602
krishivpiduri2010 <59217701+krishivpiduri2010@users.noreply.github.com>
2021-11-15 18:14:45 -0800

    Works now

--- main.py
+++ main.py
@@ -2,81 +2,40 @@ import pygame
 
 WIDTH, HEIGHT = 1500, 800
 WIN = pygame.display.set_mode((WIDTH, HEIGHT))
-square_size = 50
-grid = [[0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
-        [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
-        [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
-        [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
-        [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
-        [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
-        [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
-        [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
-        [1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1],
-        [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
-        [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
-        [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
-        [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
-        [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
-        [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
-        [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
-        [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
-        [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
-        [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
-        [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
-        [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
-        [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
-        [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
-        [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
-        [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
-        [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
-        [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
-        [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
-        [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
-        [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]]
+
+grid = tuple(tuple((0, 0, 0) if i % 2 == 0 else (255, 255, 255) for i in range(1000)) for _ in range(1000))
 
 
-def draw_grid(win: pygame.Surface):
-    for x in range(WIDTH, square_size):
-        pygame.draw.line(win, (0, 0, 0), (x, 0), (x, HEIGHT))
-    for y in range(HEIGHT, square_size):
-        pygame.draw.line(win, (0, 0, 0), (0, y), (WIDTH, y))
-
-
-def draw(win: pygame.Surface, grid_x, grid_y):
-    for row_pos, row in enumerate(range(grid_x, len(grid))):
-        for col_pos, cell in enumerate(range(grid_y, len(grid[row]))):
-            if grid[row][cell] == 1:
-                pygame.draw.rect(win, (255, 255, 255,),
-                                 (row_pos * square_size, col_pos * square_size, square_size, square_size))
-    draw_grid(win)
+def draw(win: pygame.Surface,top_right_x,top_right_y):
+    win.fill((255, 255, 255))
+    for row_num, row in enumerate(grid):
+        for col_num, cell in enumerate(row):
+            pygame.draw.rect(win, cell, [(row_num * 10) + top_right_x, (col_num * 10) + top_right_y, 10, 10])
     pygame.display.update()
 
 
 def main():
-    grid_x = 0
-    grid_y = 0
+    top_right_x = 0
+    top_right_y = 0
     run = True
     clock = pygame.time.Clock()
     while run:
-        clock.tick(10)
+        clock.tick(60)
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 run = False
                 break
         keys = pygame.key.get_pressed()
-        if keys[pygame.K_LEFT] and grid_x > 0:
-            grid_x -= 1
-        if keys[pygame.K_RIGHT]:
-            grid_x += 1
-        if keys[pygame.K_UP] and grid_y > 0:
-            grid_y -= 1
         if keys[pygame.K_DOWN]:
-            grid_y += 1
-        print(grid_x, grid_y)
-        draw(WIN, grid_x, grid_y)
-    pygame.quit()
+            top_right_y += 10
+        elif keys[pygame.K_UP]:
+            top_right_y -= 10
+        elif keys[pygame.K_RIGHT]:
+            top_right_x += 10
+        elif keys[pygame.K_LEFT]:
+            top_right_x -= 10
+        draw(WIN,top_right_x,top_right_y)
 
 
 if __name__ == '__main__':
-    print(grid)
     main()
